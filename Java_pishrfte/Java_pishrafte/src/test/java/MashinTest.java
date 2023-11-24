f332d4c59393734d22b68f0a68b47da9d7685ddf
Alireza Rezaie <alirezarezaie@eduroam-192-109-116-140.htw-saarland.de>
2023-11-24 15:20:44 +0100

    https://github.com/AliReza-Rezai/Java_pishrfte.git

--- Java_pishrfte/Java_pishrafte/src/test/java/MashinTest.java
+++ Java_pishrfte/Java_pishrafte/src/test/java/MashinTest.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class MashinTest {
+public class MashinTest {
+    class mashin{
+        boolean mashin_roshanhast;
+        boolean dar_bazehast;
+        void roshanhast(){
+            System.out.println("IS On");
+            mashin_roshanhast = true;
+        }
+
+        void khamoshhast(){
+            System.out.println("IS OFF");
+            mashin_roshanhast = false;
+        }
+        void dar_bazhast(){
+            System.out.println("Open");
+            dar_bazehast = true;
+        }
+        void dar_bastehast(){
+            System.out.println("close");
+            dar_bazehast = false;
+        }
+
+        void harekat(){
+            if(!dar_bazehast && mashin_roshanhast){
+                System.out.println("Mashine dar hale harkat");
+            } else {
+                System.out.println("Mashine dar hale park");
+            }
+        }
+    }
+
 }
